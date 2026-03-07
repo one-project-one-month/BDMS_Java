a1ff23b16f03eacacd735feb4a65a48d174534ea
thanthtooaung-coding <thanthtoo1285@gmail.com>
2026-03-07 09:22:54 +0700

    :tada: setup project

---
+++ src/main/java/com/opom/bdms/BdmsApplication.java
@@ -0,0 +1,13 @@
+package com.opom.bdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opom/bdms/BdmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opom.bdms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
